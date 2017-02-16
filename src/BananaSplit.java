3b19bea4b30bb2f017594da785481fb4c521ebb8
Munchman <league@Mac-mini-3.local>
2017-02-15 18:29:38 -0800

    for loop practice

---
+++ src/BananaSplit.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class BananaSplit {
+public static void main(String[] args) {
+	String banana ="";
+	for (int i = 1; i <= 3; i++) {
+		banana = banana+ " ice cream\n";
+	}
+	banana = banana + " banana";
+	JOptionPane.showMessageDialog(null, banana);
+	
+}
+}
