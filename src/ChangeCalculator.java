6ee351b8a40694552b7c63bfdf464ced178221c2
League <league@Mac-mini-2.local>
2017-05-03 18:28:35 -0700

    beeeeeeeeeeeeer

---
+++ src/ChangeCalculator.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class ChangeCalculator {
+	public static void main(String[] args) {
+		
+	
+	// Ask the user how many nickels they have
+	String	a=JOptionPane.showInputDialog("how many nickles do you have?");
+
+			// Convert their answer to an int using Integer.parseInt()
+
+int i =Integer.parseInt(a);
+			// Ask the user how many dimes they have, and convert their answer
+a=JOptionPane.showInputDialog("how many dimes do you have?");
+if (i >=10){
+	JOptionPane.showMessageDialog(null, "you have more than $1");
+}
+			// Ask the user how many quarters they have, and convert their answer
+
+			// Calculate how much money the user has and save it in a double variable 
+
+			// Tell the user how much money they have
+
+
+}}
---
+++ src/Fanta.java
@@ -0,0 +1,19 @@
+
+public class Fanta {
+	public static void main(String[] args) {
+		String wall="bottles of fanta on the wall";
+		String pass="take one down pass it around";
+		String fanta="bottles of fanta";
+			for (int i = 99; i >-1 ; i--) {
+				
+					System.out.println(i);
+					System.out.println(wall);
+					System.out.println(i);
+					System.out.println(fanta);
+					System.out.println(pass);
+					System.out.println(i-1);
+					System.out.println(wall);
+		
+	}
+}
+}
