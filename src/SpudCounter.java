d4cab2f2c7e09b3263da20003a2f90ad98066bff
tactreconx <league@Admins-Mac-mini.local>
2017-11-09 18:22:07 -0800

    Spud counter, modulo checkpoint and code 4 life

---
+++ src/SpudCounter.java
@@ -0,0 +1,16 @@
+
+public class SpudCounter {
+public static void main(String[] args) {
+	for (int i = 1; i <=8; i++) {
+		
+		if(i==4){
+			System.out.println("4");
+	}
+		else if(i==8){
+			System.out.println("more");
+		}
+		else {
+			System.out.println( i+" potato");
+		}
+}}
+}
